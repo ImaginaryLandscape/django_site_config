06dedbf6d0d5b136b9a7f03051324d097c731b3d
Joe Jasinski <joe.jasinski@gmail.com>
2014-07-24 03:05:44 -0500

    fixed example to be email

--- example/example/app1/__init__.py
+++ example/example/app1/__init__.py
@@ -7,7 +7,7 @@ class MyAppSiteConfig(site_config.SiteConfigBase):
     application_verbose_name = "My Application 1"
 
     def get_default_config(self):
-        return {'APP1_EMAIL':{'default':"Test A default", 'field':forms.EmailField, 'help':'Test A help text.'}, 
+        return {'APP1_EMAIL':{'default':"joe@exampe.com", 'field':forms.EmailField, 'help':'Test A help text.'}, 
                 "APP1_NUM":{'default':1, 'field':forms.IntegerField, 'help':'Test B help text.'}}
 
 site_config.settings.config_registry.register(MyAppSiteConfig)
