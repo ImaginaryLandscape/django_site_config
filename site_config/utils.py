36d355e77ebda902cfa1c2d7a0db558a3670ffda
Joe Jasinski <jjasinski@imagescape.com>
2014-07-25 14:13:48 -0500

    used deepcopy instead of copy

--- site_config/utils.py
+++ site_config/utils.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from copy import deepcopy
 from django.utils.importlib import import_module
 
 
@@ -8,7 +9,7 @@ def import_module_attr(path):
 
 
 def update_config_dict(default_config_dict, updated_config_dict):
-    config_dict = default_config_dict.copy()
+    config_dict = deepcopy(default_config_dict)
     if type(updated_config_dict) is dict:
         # update the value keys to be the contents of updated_config_dict
         for config_name, nested_config_dict in config_dict.items():
@@ -16,8 +17,9 @@ def update_config_dict(default_config_dict, updated_config_dict):
                 config_dict[config_name].update({'value':updated_config_dict[config_name]['value']})
     return config_dict
 
+
 def config_dict_value_from_default(default_config_dict):
-    new_config_dict = default_config_dict.copy()
+    new_config_dict = deepcopy(default_config_dict)
     # set a default 'value' in each nested config dict
     for config_name, x in new_config_dict.items():
         new_config_dict[config_name].update({'value':new_config_dict[config_name]['default']})
