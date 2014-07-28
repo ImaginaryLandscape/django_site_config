f107e2e11958ac15c73fa476d269997cae84e3b5
Joe Jasinski <jjasinski@imagescape.com>
2014-07-28 15:35:45 -0500

    updates to example

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.base import TemplateView
-
 from django.contrib import admin
+from . import views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='index.html'), {}, name="index"),                       
+    url(r'^$', views.HomeView.as_view(template_name='index.html'), {}, name="index"),                       
     url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<website>\w+)/foo/$', include('example.app_foo.urls')),
     url(r'^(?P<website>\w+)/bar/$', include('example.app_bar.urls')),
