91555c8413e1ce4f7da86051dce27a5be337c2a8
Joe Jasinski <jjasinski@imagescape.com>
2014-07-28 11:09:19 -0500

    This belongs to the previous commit

--- example/example/app_foo/__init__.py
+++ example/example/app_foo/__init__.py
@@ -1,35 +1,17 @@
 from django import forms
 import site_config
 
-class MyAppSiteConfig(site_config.SiteConfigBase):
+class FooConfig(site_config.SiteConfigBase):
 
-    application_slug = "app1"
-    application_verbose_name = "My Application 1"
-
-    def get_default_configs(self):
-        return {'APP1_EMAIL':{'default':"joe@exampe.com", 'field':'django.forms.EmailField', 'help':'Test A help text.'}, 
-                "APP1_NUM":{'default':1, 'field':'django.forms.IntegerField', 'help':'Test B help text.'}}
-
-site_config.settings.config_registry.register(MyAppSiteConfig)
-
-
-class MyFunConfig(site_config.SiteConfigBase):
-
-    application_slug = "myfun"
-    application_verbose_name = "My Fun App 1"
+    application_slug = "bar"
+    application_verbose_name = "Bar App"
 
     def get_default_configs(self):
-        return {'MYFUN_EMAIL':{'default':"joe@exampe.com", 
-                               'field':'django.forms.CharField', 
-                               'help':'Test A help text.'}, 
-                "MYFUN_NUM":{'default':1, 
-                             'field':'django.forms.IntegerField', 
-                             'help':'Enter a number.'},
-                "MYFUN_CHAR":{'default':"b", 
-                              'field':'django.forms.ChoiceField', 
-                              'help':'Test B help text.', 
-                              'choices':(('a',"A choice"),("b","B Choice"),)},
-                }
+        return {'FOO_EMAIL':{'default':"joe@exampe.com",
+                             'field':'django.forms.EmailField', 
+                             'help':'Test A help text.'}, 
+                "FOO_NUM":{'default':1, 
+                           'field':'django.forms.IntegerField', 
+                           'help':'Test B help text.'}}
 
-site_config.settings.config_registry.register(MyFunConfig)
-
+site_config.settings.config_registry.register(FooConfig)
