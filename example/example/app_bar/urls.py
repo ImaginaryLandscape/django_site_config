35260605eca7567938d972bc79fbe8e135ad1b9b
Joe Jasinski <jjasinski@imagescape.com>
2014-07-30 16:41:01 -0500

    added usage example for function based views.

--- example/example/app_bar/urls.py
+++ example/example/app_bar/urls.py
@@ -3,6 +3,7 @@ from site_config.decorators import enable_disable_website
 from .views import IndexView
 
 urlpatterns = [
+   url('^page3/$', 'example.app_bar.views.index', {'template_name':'app_bar/index.html'}, name="app_bar_page3"),  
    url('^page2/$', IndexView.as_view(template_name='app_bar/index.html'), {}, name="app_bar_page2"),  
    url('^page1/$', IndexView.as_view(template_name='app_bar/index.html'), {}, name="app_bar_page1"),   
    url('^$', IndexView.as_view(template_name='app_bar/index.html'), {}, name="app_bar_index"),
--- example/example/app_bar/views.py
+++ example/example/app_bar/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.base import TemplateView
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from example.app_bar import BarConfig
 
 class IndexView(TemplateView):
@@ -8,4 +10,11 @@ class IndexView(TemplateView):
         config = BarConfig(website=website)
         kwargs['config'] = config
         return kwargs
-        
+
+def index(request, template_name, website=None, *args, **kwargs):
+    config = BarConfig(website=website)
+    return render_to_response(template_name,
+                              {'config':config,},
+                              context_instance=RequestContext(request))
+    
+    
