2fde272001474915053a8f754cfceac53b39dbbc
Joe Jasinski <jjasinski@imagescape.com>
2014-08-13 14:17:35 -0500

    updates to setup to bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='site_config',
-      version='0.0.2',
+      version='0.0.3',
       description='Django configuration Utility to manage multiple "websites" in a project. ',
       author='Joe Jasinski',
       author_email='jjasinski@imagescape.com',
