d67b04582dbae60685289232d19020fbd59edd9d
Joe Jasinski <jjasinski@imagescape.com>
2014-08-13 14:04:30 -0500

    updated description

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup
 
 setup(name='site_config',
-      version='0.0.1',
-      description='Configuration Utility to manage multiple "websites" in a project. ',
+      version='0.0.2',
+      description='Django configuration Utility to manage multiple "websites" in a project. ',
       author='Joe Jasinski',
       author_email='jjasinski@imagescape.com',
       install_requires=[
