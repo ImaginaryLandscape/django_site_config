ded6869fe40ef4faae5ff88b28401cf59cdc3569
David Bertrand <dbertrand@imagescape.com>
2015-02-26 20:03:16 +0000

    Added website object to context processor

--- site_config/context_processors.py
+++ site_config/context_processors.py
@@ -1,24 +1,34 @@
 from __future__ import unicode_literals
 from site_config.utils import website_override_template
+from site_config.backends.model_backend.models import Website
 from django.conf import settings
+
 
 
 def decide_base_template(request):
     base_name = getattr(
         settings, 'SITECONFIG_BASE_TEMPLATE', 'base_site.html'
-    )
+    )    
+    website_obj = None
     try:
         website = request.resolver_match.kwargs.get('website', None)
     except:
         website = None
+
     if website:
         base_name = website_override_template(base_name, website).name
+        try:
+            website_obj = Website.objects.get(short_name=website)
+        except Website.DoesNotExist as e:
+            website_obj = None       
     else:
         parts = [part for part in request.path.split('/') if part != ""]
         if len(parts) > 0:
             website = parts[0]
             base_name = website_override_template(base_name, website).name
+
     return {
         'base_template': base_name,
-        'website_shortname': website
+        'website_shortname': website,
+        'website': website_obj
     }
