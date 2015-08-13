59ab75d0edb0441c64e51a98e4df923c5e9f9163
Dan Johnson <djohnson@imagescape.com>
2015-08-13 10:16:23 -0500

    Add website_name to context

--- site_config/context_processors.py
+++ site_config/context_processors.py
@@ -4,12 +4,12 @@ from site_config.backends.model_backend.models import Website
 from django.conf import settings
 
 
-
 def decide_base_template(request):
     base_name = getattr(
         settings, 'SITECONFIG_BASE_TEMPLATE', 'base_site.html'
-    )    
+    )
     website_obj = None
+    website_name = None
     try:
         website = request.resolver_match.kwargs.get('website', None)
     except:
@@ -17,7 +17,7 @@ def decide_base_template(request):
 
     if website:
         base_name = website_override_template(base_name, website).name
-    else:        
+    else:
         parts = [part for part in request.path.split('/') if part != ""]
         if len(parts) > 0:
             website = parts[0]
@@ -26,11 +26,13 @@ def decide_base_template(request):
     if website:
         try:
             website_obj = Website.objects.get(short_name=website)
+            website_name = website_obj.name
         except Website.DoesNotExist as e:
             website_obj = None
 
     return {
         'base_template': base_name,
         'website_shortname': website,
+        'website_name': website_name,
         'website': website_obj
     }
