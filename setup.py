3af29eadbed4b74e35e7fa7e5c876bba2d221b63
Steven Hemmy <shemmy@imagescape.com>
2021-02-27 21:04:26 -0600

    fix long description in setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     version='1.1.0',
     description='Django configuration Utility to manage multiple "websites" in a project. ',
     long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     author='Joe Jasinski',
     author_email='jjasinski@imagescape.com',
     install_requires=[
