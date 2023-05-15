ImaginaryLandscape/django_site_config

commit 716d28b4802ff5a7575e3dd18b3d05d8cc6145fb

remove code that (unintentionally?) blanked values not represented in the changelist when saving directly from the changelist using the 'save' button at the bottom

site_config/backends/model_backend/admin.py
@@ -112,9 +112,6 @@ class WebsiteApplicationAdmin(admin.ModelAdmin):
             )
             default_config = config_lookup[1]().get_default_configs()
             obj.set_config_options(utils.config_dict_value_from_default(default_config), save=False)
-        else:
-            obj.set_config_options(
-                form.cleaned_data.get('options', {}), save=False)
         obj.save()
 
 

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "site-config"
-version = "2.0.1"
+version = "2.0.2"
 description = 'Django configuration Utility to manage multiple "websites" in a project.'
 homepage = "https://github.com/ImaginaryLandscape/django_site_config"
 readme = "README.md"

commit 24c24870ff00baae5f93b2810186f86233985f6a

Reverse commit 716d28b, which broke the ability to save changes from the changeview. However, that commit was trying to address a bug when saving from the listview, so now we handle both cases correctly

site_config/backends/model_backend/admin.py
@@ -110,12 +110,16 @@ class WebsiteApplicationAdmin(admin.ModelAdmin):
         return backend_forms.website_application_formfactory(instance=obj)
 
     def save_model(self, request, obj, form, change):
+        config_lookup = registry.config_registry.get_config_class(obj.application.short_name)
+        default_config = config_lookup[1]().get_default_configs()
         if obj.id is None:
-            config_lookup = registry.config_registry.get_config_class(
-                obj.application.short_name
-            )
-            default_config = config_lookup[1]().get_default_configs()
-            obj.set_config_options(utils.config_dict_value_from_default(default_config), save=False)
+            # This branch adds default values to each config option
+            config_dict = utils.config_dict_value_from_default(default_config)
+        else:
+            # This branch updates the current values with the form values
+            config_dict = obj.get_config_options(default_config)
+            config_dict.update(form.cleaned_data.get('options', {}))
+        obj.set_config_options(config_dict, save=False)
         obj.save()
 
 