6e4d048863669335fccd86bf377ea55503bf5ad3
Noel Taylor <ntaylor@imagescape.com>
2023-09-06 16:32:05 -0500

    remove outdated and/or non-functional 'example' and 'tests' directories

--- example/example/app_foo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from site_config.decorators import enable_disable_website
-from .views import IndexView
-
-urlpatterns = [
-   url('^$', IndexView.as_view(template_name='app_foo/index.html'), {}, name="app_foo_index")
-]
